40b68cdc549ed16001703816fb4e0a6b39ae6070
Aleksandra Arsic <arsic.aleksandra83@gmail.com>
2022-04-10 13:08:48 +0200

    Dodat korisnikRepository + testiranje

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import com.example.demo.entity.Korisnik;
+import com.example.demo.entity.Kupac;
+import com.example.demo.entity.Dostavljac;
+
+import com.example.demo.repository.KorisnikRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,8 +13,48 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
 
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+
+	@Autowired
+	private KorisnikRepository korisnikRep;
+
+	@Override
+	public void run(String... args) {
+
+		//  kreiramo novi objekat klase Korisnik
+		Korisnik k = new Korisnik();
+
+		k.setIme("Jovanka");
+		k.setPrezime("Jovkić");
+		k.setUloga("Kupac");
+		k.setP("Z");
+		k.setKorisnickoIme("jj_742");
+		k.setLozinka("******");
+
+
+		Kupac kupac1 = new Kupac();
+		kupac1.setIme("Marko");
+		kupac1.setPrezime("Markovic");
+		kupac1.setUloga("Kupac");
+		kupac1.setP("M");
+		kupac1.setKorisnickoIme("jj_742");
+		kupac1.setLozinka("******");
+		kupac1.setBrojSakupljenihBodova(5);
+
+		// čuvamo objekate u bazi
+		this.korisnikRep.save(k);
+		this.korisnikRep.save(kupac1);
+
+		List<Korisnik> korisnici = this.korisnikRep.findAll();
+
+		for (Korisnik kk : korisnici){
+			System.out.println(kk);
+		}
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
