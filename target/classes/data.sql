6efbee0ea0fbc81ad0ec465bff07fe938cc7d025
Luka Petrovic <petrovicluka73@gmail.com>
2022-04-12 19:23:48 +0200

    Update: Restoran i properti aplikacije
    U restoranu fetch type promenjen na eager. Valjda se artikli ne ucitavaj na vreme. U property dodata naredba koja ce spreciti LazyInitializationException. Ziveo stackoverflow!

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,6 +1,10 @@
 /*lokacije*/
-INSERT INTO LOKACIJA(adresa, geografska_duzina,geografska_sirina) VALUES ( 'adresa1', 123, 232);
-INSERT INTO LOKACIJA(adresa, geografska_duzina,geografska_sirina) VALUES ( 'adresa2', 123, 232);
+INSERT INTO LOKACIJA(adresa, geografska_duzina,geografska_sirina) VALUES ( 'Trg Mladenaca 11', 123, 232);
+INSERT INTO LOKACIJA(adresa, geografska_duzina,geografska_sirina) VALUES ( 'Maksima Gorkog 22', 223, 432);
+INSERT INTO LOKACIJA(adresa, geografska_duzina,geografska_sirina) VALUES ( 'Despota Stefana 7A', 1223, 2432);
+INSERT INTO LOKACIJA(adresa, geografska_duzina,geografska_sirina) VALUES ( 'Mise Dimitrijevica', 23, 42);
+INSERT INTO LOKACIJA(adresa, geografska_duzina,geografska_sirina) VALUES ( 'Dositeja Obradovica 4B', 293, 932);
+INSERT INTO LOKACIJA(adresa, geografska_duzina,geografska_sirina) VALUES ( 'Vuka Karadzica', 1, 3);
 
 /*restorani*/
 INSERT INTO RESTORAN(naziv, tip_restorana, id_lokacije) VALUES ('BlueMoon','Restaurant & Bar', 2);
@@ -19,4 +23,4 @@ INSERT INTO KORISNIK (dtype, korisnicko_ime, lozinka,ime,prezime, pol, datum_rod
 
 INSERT INTO KOMENTAR(ocena, tekst_komentara, id_kupca, id_restorana) VALUES(10, 'Dobar restoran', 1, 1);
 
-INSERT INTO KORISNIK (dtype, korisnicko_ime, lozinka,ime,prezime, pol, datum_rodjenja,uloga) VALUES ('Menadzer','M_Arsa','jasamsifra123','Arsa','Arsic','Z','2001-02-02','MENADZER');
+INSERT INTO KORISNIK (dtype, korisnicko_ime, lozinka,ime,prezime, pol, datum_rodjenja,uloga, id_restorana) VALUES ('Menadzer','M_Arsa','jasamsifra123','Arsa','Arsic','Z','2001-02-02','MENADZER', 1);
