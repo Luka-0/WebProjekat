7cf8ea0387708108b1942a0b05cc0b44e950e059
Luka Petrovic <petrovicluka73@gmail.com>
2022-04-24 00:10:34 +0200

    Dodata Repository anotacija svim repozitorijumima

--- src/main/java/com/example/demo/repository/LokacijaRepository.java
+++ src/main/java/com/example/demo/repository/LokacijaRepository.java
@@ -2,9 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Lokacija;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface LokacijaRepository extends JpaRepository<Lokacija, Long>{
 
     List<Lokacija> findAllByAdresaOrderById(String adresa);
